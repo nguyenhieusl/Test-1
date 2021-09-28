45efa9c7a1a5532250700a4fc1c760d403edabc6
unknown <nguyenhieuvf@gmail.com>
2021-09-28 20:25:53 +0700

    update information Nguyen Huu Hieu

---
+++ displayInformation.py
@@ -0,0 +1,5 @@
+print("Name: Nguyễn Hữu Hiếu")
+print("Date of birth: 23/07/1991")
+print("Hometown: Phù Yên - Son La")
+print("Currently living: Long Bien - Hà Nội")
+print("Workplace: Nhà Máy Vinfast tại Hải Phòng")
